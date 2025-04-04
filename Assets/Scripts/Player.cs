565597bb961b3c1caeead6627dde58701cb59a5d
WuulJi <eragonrain@gmail.com>
2025-04-04 11:12:30 +0800

    修改Player.cs
    新增按鈕 : 點擊按鈕或 SPACE 繼續遊戲
    新增 Die()、Replay()
    修改hp判斷 : hp<=0
    replayButton 初始化為 false

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,8 @@
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -14,20 +16,22 @@ public class Player : MonoBehaviour
      Animator anim;
      SpriteRenderer sprR;
      AudioSource deathSound;
+     [SerializeField] GameObject replayButton;
 
      // Start is called once before the first execution of Update after the MonoBehaviour is created
      void Start()
-    {
+     {
           hp = 10;
           score = 0;
           anim = GetComponent<Animator>();
           sprR = GetComponent<SpriteRenderer>();
           deathSound = GetComponent<AudioSource>();
+          replayButton.SetActive(false);
      }
 
-    // Update is called once per frame
-    void Update()
-    {
+     // Update is called once per frame
+     void Update()
+     {
           if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
           {
                transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
@@ -44,15 +48,27 @@ public class Player : MonoBehaviour
           {
                anim.SetBool("Move", false);
           }
+
+          // 繼續遊戲的按鈕出現才檢查空白鍵的觸發，也就是死了才檢查
+          if (replayButton.activeSelf == true)
+          {
+               // 檢查空白鍵是否被按下
+               if (Input.GetKeyDown(KeyCode.Space))
+               {
+                    // 手動調用按鈕的 onClick 事件
+                    replayButton.GetComponent<Button>().onClick.Invoke();
+                    // 功能等同於直接呼叫 Replay()
+               }
+          }          
      }
 
      private void OnCollisionEnter2D(Collision2D collision)
      {
-          if (collision.gameObject.tag == "Normal") 
+          if (collision.gameObject.tag == "Normal")
           {
                if (collision.contacts[0].normal == Vector2.up)
                {
-                    Debug.Log("hit Normal");
+                    //Debug.Log("hit Normal");
 
                     if (currentFloor == null)// 如果 currentFloor 為空，表示是第一次碰到地板，初始化 currentFloor 和 lastFloor
                     {
@@ -64,7 +80,7 @@ public class Player : MonoBehaviour
                          lastFloor = currentFloor;
                          currentFloor = collision.gameObject;
                          int floorsJumped = Mathf.Abs(Mathf.FloorToInt(currentFloor.transform.position.y - lastFloor.transform.position.y));
-                         Debug.Log("floorsJumped: " + floorsJumped);
+                         //Debug.Log("floorsJumped: " + floorsJumped);
                          UpdateScore(floorsJumped);
                     }
 
@@ -72,11 +88,11 @@ public class Player : MonoBehaviour
                     collision.gameObject.GetComponent<AudioSource>().Play();
                }
           }
-          else if(collision.gameObject.tag == "Nails")
+          else if (collision.gameObject.tag == "Nails")
           {
                if (collision.contacts[0].normal == Vector2.up)
                {
-                    Debug.Log("hit Nails");
+                    //Debug.Log("hit Nails");
 
                     if (currentFloor == null) // 如果 currentFloor 為空，表示是第一次碰到地板，初始化 currentFloor 和 lastFloor
                     {
@@ -88,19 +104,20 @@ public class Player : MonoBehaviour
                          lastFloor = currentFloor;
                          currentFloor = collision.gameObject;
                          int floorsJumped = Mathf.Abs(Mathf.FloorToInt(currentFloor.transform.position.y - lastFloor.transform.position.y));
-                         Debug.Log("floorsJumped: " + floorsJumped);
+                         //Debug.Log("floorsJumped: " + floorsJumped);
                          UpdateScore(floorsJumped);
                     }
 
                     ModifyHp(-1);
-                    
+
                     anim.SetTrigger("Hurt");
                     collision.gameObject.GetComponent<AudioSource>().Play();
                }
           }
-          else if(collision.gameObject.tag == "Ceiling")
+          else if (collision.gameObject.tag == "Ceiling")
           {
-               Debug.Log("hit Ceiling");
+               //Debug.Log("hit Ceiling");
+
                currentFloor.GetComponent<BoxCollider2D>().enabled = false;
                ModifyHp(-1);
 
@@ -113,18 +130,17 @@ public class Player : MonoBehaviour
      {
           if (collision.gameObject.tag == "DeathLine")
           {
-               Debug.Log("hit Dead");
-               deathSound.Play();
+               Die();
           }
      }
 
-     void ModifyHp (int num)
+     void ModifyHp(int num)
      {
           hp += num;
-          if (hp < 0) 
-          { 
+          if (hp <= 0)
+          {
                hp = 0;
-               deathSound.Play();
+               Die();
           }
           else if (hp > 10) hp = 10;
           UpdateHp();
@@ -148,7 +164,22 @@ public class Player : MonoBehaviour
      public void UpdateScore(int floorsJumped)
      {
           score += floorsJumped;
-          scoreText.text = "地下 " +score+ " 層"; 
+          scoreText.text = "地下 " + score + " 層";
           //用score.ToString()結果一樣，C#會自動轉字串
+     }
+
+     void Die()
+     {
+          deathSound.Play();
+          Time.timeScale = 0; //暫停遊戲
+          replayButton.SetActive(true);          
+     }
+
+     public void Replay()
+     {
+          Time.timeScale = 1; //恢復遊戲
+          SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+          //Debug.Log(SceneManager.GetActiveScene().name);
+          //SceneManager.GetActiveScene().name == 當前場景 == "SampleScene"
      }
 }
